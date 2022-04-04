1f5f75e755d0de3e3e5df137078c521b0d217053
Ayresia <nalizyoncatania0@gmail.com>
2022-04-04 13:03:55 +0200

    feat: setup bot

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
+use teloxide::prelude2::*;
+use dotenv::dotenv;
+use anyhow::Result;
+
 #[tokio::main]
-async fn main() {
-    todo!();
+async fn main() -> Result<()> {
+    dotenv().ok();
+
+    let channel_id = std::env::var("CHANNEL_ID")?;
+    let bot = Bot::from_env().auto_send();
+
+    Ok(())
 }
