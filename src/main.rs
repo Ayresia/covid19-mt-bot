66db3f32397133268be5468b8a3d8ce6ad609b78
Ayresia <nalizyoncatania0@gmail.com>
2022-04-05 00:58:44 +0200

    fix(main): main loop date checking

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod covid;
 
 use anyhow::Result;
+use chrono::{NaiveDate, Local};
 use dotenv::dotenv;
 use std::{thread, time::Duration};
 use teloxide::{
@@ -14,20 +15,26 @@ async fn main() -> Result<()> {
 
     let channel_id = std::env::var("CHANNEL_ID").unwrap();
     let bot = Bot::from_env().auto_send();
-    let mut is_sent = false;
+    let mut last_sent_date: Option<NaiveDate> = None;
 
     loop {
-        let latest_case = covid::fetch_latest_case().await?;
+        let current_date = Local::now().date().naive_local();
 
-        if !is_sent {
-            bot.send_message(channel_id.clone(), latest_case.into_pretty_string())
-                .await?;
+        if last_sent_date.is_none() {
+            let latest_case = covid::fetch_latest_case().await?;
 
-            is_sent = true;
-        } else if is_sent {
-            is_sent = false;
+            if latest_case.date == current_date {
+                bot.send_message(channel_id.clone(), latest_case.into_pretty_string())
+                    .await?;
+
+                last_sent_date = Some(current_date);
+            }
+        } else if let Some(date) = last_sent_date {
+            if date != current_date {
+                last_sent_date = None;
+            }
         }
 
-        thread::sleep(Duration::from_secs(60));
+        thread::sleep(Duration::from_secs(5));
     }
 }
